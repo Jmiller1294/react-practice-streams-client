a5758e1813685e1eb6efb329700a0392ff147a1f
Justin Miller <jayman1294@gmail.com>
2021-07-15 23:07:02 -0400

    added routes for components

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import StreamCreate from './streams/StreamCreate';
+import StreamDelete from './streams/StreamDelete';
+import StreamEdit from './streams/StreamEdit';
+import Streamlist from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
 
-const PageOne = () => {
-  return <div>PageOne</div>
-}
-
-const PageTwo = () => {
-  return <div>PageTwo</div>
-}
 
 const App = () => {
   return (
     <div>
       <Router>
-        <Route path="/" exact component={PageOne} />
-        <Route path="/pagetwo" exact component={PageTwo} />
+        <Route path="/" exact component={Streamlist} />
+        <Route path="/streams/new" exact component={StreamCreate} />
+        <Route path="/streams/edit" exact component={StreamEdit} />
+        <Route path="/streams/delete" exact component={StreamDelete} />
+        <Route path="/streams/show" exact component={StreamShow} />
       </Router>
     </div>
   )
